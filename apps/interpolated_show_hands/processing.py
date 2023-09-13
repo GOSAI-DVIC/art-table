a40d86415b750c964d732372d2acbb67c03a2599
Thomas-Jld <thomas.juldo@edu.devinci.fr>
2023-09-13 19:05:30 +0200

    Removing hand signs for interpolated and using full body pose

--- apps/interpolated_show_hands/processing.py
+++ apps/interpolated_show_hands/processing.py
@@ -7,9 +7,9 @@ class Application(BaseApplication):
 
     def __init__(self, name, hal, server, manager):
         super().__init__(name, hal, server, manager)
-        # self.requires["pose"] = ["raw_data"]
-        self.requires["hand_pose"] = ["raw_data"]
-        self.requires["hand_sign"] = ["sign"]
+        self.requires["pose"] = ["raw_data"]
+        # self.requires["hand_pose"] = ["raw_data"]
+        # self.requires["hand_sign"] = ["sign"]
         self.requires["interpolate"] = ["interpolated_data"]
         self.hand_pose_data = {}
         self.interpolation = {
@@ -37,7 +37,15 @@ class Application(BaseApplication):
 
         if source == "pose" and event == "raw_data" and data is not None:
             self.hand_pose_data["hands_handedness"] = [["right", 1], ["left", 1]]
-            self.interpolation["points"] = [data["right_hand_pose"], data["left_hand_pose"]]
+            right_hand = data["right_hand_pose"]
+            left_hand = data["left_hand_pose"]
+            for i in range(len(right_hand)):
+                right_hand[i][0] = right_hand[i][0] / 640
+                right_hand[i][1] = right_hand[i][1] / 480
+            for i in range(len(left_hand)):
+                left_hand[i][0] = left_hand[i][0] / 640
+                left_hand[i][1] = left_hand[i][1] / 480
+            self.interpolation["points"] = [right_hand, left_hand]
             self.execute(
                 "interpolate",
                 "interpolate_points",
@@ -56,5 +64,5 @@ class Application(BaseApplication):
         ):
             self.hand_pose_data["hands_landmarks"] = data["points"]
             self.hand_pose_data["emit_time"] = time.time()
-            if "hands_sign" in self.hand_pose_data:
-                self.server.send_data(self.name, self.hand_pose_data)
+            # if "hands_sign" in self.hand_pose_data:
+            self.server.send_data(self.name, self.hand_pose_data)
